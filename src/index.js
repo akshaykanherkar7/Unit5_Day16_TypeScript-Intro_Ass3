079f38ace15798698e155501ae965799e8b29bcc
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-07-13 15:15:08 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-var findWhich = {
-    type: User,
+function getUserType(obj) {
+    return obj.type;
+}
+var Person1 = {
+    type: "user",
     name: "Akshay",
+    age: 26,
+    occupation: "full-stact"
+};
+var Person2 = {
+    type: "admin",
+    name: "Admin",
     age: 26,
     role: "full-stact"
 };
+console.log(getUserType(Person1));
+console.log(getUserType(Person2));
--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,23 @@ interface Admin {
     role: string;
 }
 
-let findWhich : (User & Admin) = {
-    type: User,
+function getUserType(obj : User | Admin):string{
+    return obj.type
+}
+
+let Person1 : User = {
+    type: "user",
     name: "Akshay",
+    age: 26,
+    occupation: "full-stact"
+}
+
+let Person2 : Admin = {
+    type: "admin",
+    name: "Admin",
     age: 26,
     role: "full-stact"
 }
+
+console.log(getUserType(Person1))
+console.log(getUserType(Person2)) 
